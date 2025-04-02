818b20ade361f41dd19c5f53459b0917c4270a25
eugenetwtw <539663@gmail.com>
2025-04-02 16:21:17 +0800

    Initialize Next.js project with essential configuration and dependencies

---
+++ .next/static/HFIMlX02GpL2lQPgOu5h8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:void 0,source:e,destination:e}],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-095a012fc890179d.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]}}("/api/:path*"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
