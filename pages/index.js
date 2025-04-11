e9c9bb776177a07e447b7b9ebd0f06c68941afe2
eugenetwtw <539663@gmail.com>
2025-04-11 18:19:44 +0800

    Add Analytics component to _app.js and update index.js for redirection

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-// This file is needed for Vercel to recognize the project as a Next.js application
-// It will redirect to the static index.html file
-
 import { useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   useEffect(() => {
@@ -13,6 +11,7 @@ export default function Home() {
     <div>
       <h1>重定向中...</h1>
       <p>如果您看到此頁面，請點擊<a href="/index.html">這裡</a>繼續。</p>
+      <Analytics />
     </div>
   );
 }
